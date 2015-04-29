3fd1fe521e9f16dfbf68be91727cb58d1c145897
sacer6 <jonatan.d.g@hotmail.com>
2015-04-29 19:29:00 +0200

    cnx database

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'host'      => 'josep-portatil',
+			'database'  => 'dawtrello',
+			'username'  => 'root',
+			'password'  => 'Arroba2011',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
