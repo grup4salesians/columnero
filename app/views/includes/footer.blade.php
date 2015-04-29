d0fb26872307a25e52c75e78268d6a356cdfab3e
Josep Sarabia <josep.sarabia@gmail.com>
2015-04-29 18:29:58 +0200

    no message

---
+++ app/views/includes/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2015 DawTrello | By Grup 4
+                </div>
+
+            </div>
+        </div>
+    </footer>
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,29 +1,16 @@
 <html>
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <!--[if IE]>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-            <![endif]-->
-        <title>{{ $title }}</title>
-        <!-- BOOTSTRAP CORE STYLE  -->
-        <link href="assets/css/bootstrap.css" rel="stylesheet" />
-        <!-- FONT AWESOME ICONS  -->
-        <link href="assets/css/font-awesome.css" rel="stylesheet" />
-        <!-- CUSTOM STYLE  -->
-        <link href="assets/css/style.css" rel="stylesheet" />
-        <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        <title>@yield('title','Default Content')</title>
+        @include('includes.head')
     </head>
     <body>
-        <div class="container">
-            @yield('content')
+        @include('includes.header');
+        <div class='header'>
+            @include('includes.header')
+        </div>
+        @yield('content)
+        <div class='footer'>
+            @include('includes.footer')
         </div>
     </body>
 </html>
