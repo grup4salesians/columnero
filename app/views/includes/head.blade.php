87ab1e267928c85cd2db039395f5850aa58fd248
Josep Sarabia <josep.sarabia@gmail.com>
2015-04-29 19:29:39 +0200

    Css añadidos en Public/css y en Header.

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -8,11 +8,9 @@
         <![endif]-->
     <title>Free Responsive Admin Theme - ZONTAL</title>
     <!-- BOOTSTRAP CORE STYLE  -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONT AWESOME ICONS  -->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- CUSTOM STYLE  -->
-    <link href="assets/css/style.css" rel="stylesheet" />
+    {{ HTML::style('css/bootstrap.css'); }}
+    {{ HTML::style('css/style.css'); }}
+    {{ HTML::style('css/font-awesome.css'); }}
      <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
