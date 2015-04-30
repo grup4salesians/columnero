dffe31610081dac9ae4d60a939ca8e46e004d490
vektrom <kenzo_aku@hotmail.com>
2015-04-30 16:26:12 +0200

    Arreglando plantilla

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -1,21 +1,18 @@
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <!--[if IE]>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <![endif]-->
-    <title>Free Responsive Admin Theme - ZONTAL</title>
-    <!-- BOOTSTRAP CORE STYLE  -->
-    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-    {{ HTML::style('css/bootstrap.css'); }}
-    {{ HTML::style('css/style.css'); }}
-    {{ HTML::style('css/font-awesome.css'); }}
-     <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+<!--[if IE]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <![endif]-->
-</head>
+<!-- BOOTSTRAP CORE STYLE  -->
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+{{ HTML::style('css/bootstrap.css'); }}
+{{ HTML::style('css/style.css'); }}
+{{ HTML::style('css/font-awesome.css'); }}
+ <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
