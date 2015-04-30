488e08a3e1671b20d4dabcaaf053e17cb5bd3935
Josep Sarabia <josep.sarabia@gmail.com>
2015-04-30 17:05:59 +0200

    Función logout y header modificado para menú diferente cuando haya login. (falta rellenar menú).

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -8,7 +8,19 @@
         </div>
     </header>
 <div class='header_login' style="text-align:center;"> 
+        <?php if (Auth::check()) { //COMPRUEBA SI HAY USUARIO CONECTADO, SI LO HAY, INDICA UN MENÚ NUEVO Y UN CERRAR SESIÓN?> 
+                    <li role="presentation" class="dropdown">
+                        <a id="dLabel" href="#" data-toggle="dropdown" aria-haspopup="true" role="button" style="color:white;">
+                            <?php echo Auth::user()->getFullNameAttribute(); ?> <span class="caret"></span>
+                        </a>
+                        
+
+                    </li>
+     <div id='iniciarsessio' onclick="location.href='../public/logout'"><span class="glyphicon glyphicon-lock" style="font-size:30px;display:block;"></span> Tancar Sessió</div>
+
+                <?php }else{  //NO HAY USUARIO POR LO QUE INDICA EL MENÚ NORMAL, REGISTRO & INICIAR SESSIÓN?>
      <div id='registrarse'  onclick="location.href='../public/registro'"> <span class="glyphicon glyphicon-user" style="font-size:30px;display:block;"></span> Registrarse </div> 
      <div id='iniciarsessio' onclick="location.href='../public/login'"><span class="glyphicon glyphicon-lock" style="font-size:30px;display:block;"></span> Iniciar sessió</div>
+               <?php }?>
    
 </div>
