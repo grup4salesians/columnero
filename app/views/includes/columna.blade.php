a063b8639d1f4f85329250a4756ce80835e982f1
vektrom <kenzo_aku@hotmail.com>
2015-05-04 20:31:09 +0200

    Columna

--- app/views/includes/columna.blade.php
+++ app/views/includes/columna.blade.php
@@ -1,11 +1,13 @@
 <div class="col-md-4">
-    <div class="panel panel-default">
+    <div class="panel columna panel-primary">
         <div class="panel-heading">
             {{ $nom }}
         </div>
 
         <div class="panel-body">
-            Notas
+            @include('includes/nota')
+            @include('includes/nota')
+            @include('includes/nota')
         </div>
     </div>
 </div> 
