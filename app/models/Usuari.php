1f0a55f8b1cf10f5801b4443125179e2af69b66f
sacer6 <jonatan.d.g@hotmail.com>
2015-05-04 17:07:24 +0200

    holaaaaaaaaaaaaaaaaa

--- app/models/Usuari.php
+++ app/models/Usuari.php
@@ -4,7 +4,7 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
-
+ 
 class Usuari extends Eloquent implements UserInterface, RemindableInterface {
 
     use UserTrait, RemindableTrait;
