b446b6ea305f869fffc54fe16fdf063fde571097
vektrom <kenzo_aku@hotmail.com>
2015-05-04 16:42:58 +0200

    ale

---
+++ app/views/includes/columna.blade.php
@@ -0,0 +1,11 @@
+<div class="col-md-4">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            {{ $nom }}
+        </div>
+
+        <div class="panel-body">
+            Notas
+        </div>
+    </div>
+</div> 
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -3,8 +3,12 @@
 Home
 @stop
 @section('content')
-<div id="contingut_home">
-    
-</div>
-
+	<div id="contingut_home">
+		<div class="row">
+			<?php $nom = Auth::user()->getFullNameAttribute(); ?>
+			@include('includes/columna')
+			@include('includes/columna')
+			@include('includes/columna')
+		</div>
+	</div>
 @stop
