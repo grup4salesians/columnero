4e54597f1f91dfc563292f9d8775571ac47aeb25
arnaupamies <apamies94@outlook.com>
2015-05-04 16:55:31 +0200

    no message

---
+++ app/controllers/RegistreController.php
@@ -0,0 +1,60 @@
+<?php
+
+class RegistreController extends BaseController {
+
+    public function showFormulari() {
+        if (Auth::check()) {
+            return Redirect::to('/');
+        }
+        return View::make('pages.registro');
+    }
+
+    public function postRegistre() {
+
+        $userdata = array(
+            'nom' => Input::get('nom'),
+            'cognom' => Input::get('cognoms'),
+            'email' => Input::get('correu'),
+            'password' => Input::get('password'),
+            'contrasenya_confirm' => Input::get('contrasenya_confirm'),
+            'nick' => Input::get('nick')
+        );
+        $rules = [
+            'nom' => 'required|min:1',
+            'cognom' => 'required|min:1',
+            'email' => 'required|email|unique:usuaris',
+            'password' => 'required|min:6',
+            'contrasenya_confirm' => 'required|same:password',
+            'nick' => 'required|min:6'
+        ];
+        $validator = Validator::make($userdata, $rules);
+        if ($validator->fails()) {
+            return Redirect::back()->withInput()->withErrors($validator);
+        }
+       
+        $usuari = new Usuari();
+        $usuari->nom = Input::get('nom');
+        $usuari->cognom = Input::get('cognoms');
+        $usuari->email = Input::get('correu');
+        $usuari->contrasenya = Hash::make(Input::get('password'));
+        $usuari->nick = Input::get('nick');
+        $usuari->save();
+
+//                Mail::send('emails.template', array('firstname' => Input::get('nom')), function ($message) {
+//                    $message->subject('DawSharing04');
+//                    $message->to(Input::get('correu'));
+//                });
+
+        $userdata = array(
+            'email' => Input::get('correu'),
+            'password'=> Input::get('password')
+        );
+
+        if (Auth::attempt($userdata,1)) {
+            return Redirect::to('/');
+        }
+    }
+
+}
+
+?>
--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -11,4 +11,46 @@
      <div id='registrarse'  onclick="location.href='../public/registro'"> <span class="glyphicon glyphicon-user" style="font-size:30px;display:block;"></span> Registrarse </div> 
      <div id='iniciarsessio' onclick="location.href='../public/login'"><span class="glyphicon glyphicon-lock" style="font-size:30px;display:block;"></span> Iniciar sessió</div>
    
+            <div id='cerrarsession'>
+                   <a tabindex="-1" href="<?php echo Config::get('constants.BaseUrl') ?>public/logout"> 
+                  <span class="glyphicon glyphicon-lock iconosHeader">
+             
+                    </span>
+                    Tancar Sessió
+                </a>
+
+            </div>
+    
+     <div id='usuario'> 
+         
+        
+                <a id="dLabel" href="#" data-toggle="dropdown" aria-haspopup="true" role="button" style="float:right;display: inline-block;">
+                   <span class="glyphicon glyphicon-user iconosHeader"></span>
+                    <?php echo Auth::user()->getFullNameAttribute(); ?> <span class="caret"></span>
+                    <div class="hidden_area"> 
+                    <ul>
+                        <li>Hola</li>
+                        <li>Halop</li>
+                        <li>Hailop</li>
+                    </ul>
+                        </div>
+                </a>
+           
+        </div>
+        
+    <?php } else {  //NO HAY USUARIO POR LO QUE INDICA EL MENÚ NORMAL, REGISTRO & INICIAR SESSIÓN?>
+        <div id='registrarse'> 
+            <a tabindex="-1" href="<?php echo Config::get('constants.BaseUrl') ?>public/registro">
+                <span class="glyphicon glyphicon-user iconosHeader"></span>
+                Registre
+            </a>
+        </div>
+        <div id='iniciarsessio'> 
+            <a tabindex="-1" href="<?php echo Config::get('constants.BaseUrl') ?>public/login">
+                <span class="glyphicon glyphicon-lock iconosHeader"></span>
+                Iniciar Sessió
+            </a>
+        </div>
+    <?php } ?>
+
 </div>
