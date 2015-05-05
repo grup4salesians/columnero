267fa9f51cdf849dcd0dff8dc48491c6563c7667
Josep Sarabia <josep.sarabia@gmail.com>
2015-05-05 16:58:46 +0200

    Añadido buscador

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -3,13 +3,51 @@
 Home
 @stop
 @section('content')
-	<div id="contingut_home">
-		<div class="row">
-			<?php $nom = Auth::user()->getFullNameAttribute(); ?>
-			@include('includes/columna')
-			@include('includes/columna')
-			@include('includes/columna')
-			@include('includes/columna')
-		</div>
-	</div>
+<style>
+    .titulsbusqueda{
+        margin-left:10px;
+        font-weight:bold;
+    }
+    .btnfiltrar{
+        float:right;
+        padding-right:5px;
+        padding-left:5px;
+        background-color: blue;
+        margin-right:5px;
+    }
+    @media screen and (max-width: 770px) {
+    #ordenar_home{
+        width:90%;
+       height:130px;
+    }
+    #busqueda_home{
+        width:100%;
+        height:160px;
+    }
+}
+</style>
+<div id="contingut_home">
+    <div id='busqueda_home'>
+        <div id='ordenar_home'>
+             {{ Form::open(array('url' => '/cercarhome')) }}
+                <fieldset>
+                    <legend style='margin-left:5px;'>Ordenar per</legend>
+                    <span class='titulsbusqueda'>Millor valorats</span> <input  type="checkbox">
+                    <span class='titulsbusqueda'>Tots</span> <input  name="radio1" checked="checked" id="filtrocheckbox" type="radio">
+                    <span class='titulsbusqueda'>Setmana</span> <input name="radio1" id="filtrocheckbox" type="radio">
+                    <span class='titulsbusqueda'>Mes</span> <input name="radio1" id="filtrocheckbox"  type="radio">
+                    <input  style="margin-left:20px;" placeholder="tag1,tag2.." type="text">
+                    <input type="submit" class="btn btn-default" value="Enviar">
+                </fieldset>
+           {{ Form::close() }}
+        </div>
+    </div>
+    <div class="row">
+        <?php $nom = Auth::user()->getFullNameAttribute(); ?>
+        @include('includes/columna')
+        @include('includes/columna')
+        @include('includes/columna')
+        @include('includes/columna')
+    </div>
+</div>
 @stop
