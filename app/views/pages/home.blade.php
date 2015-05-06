dbd6f264d9588455dcddcde93c0b371d89daa9c2
vektrom <kenzo_aku@hotmail.com>
2015-05-06 16:33:21 +0200

    Toggle sort

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -16,22 +16,41 @@ Home
 	        margin-right:5px;
 	    }
 	    @media screen and (max-width: 770px) {
-	    #ordenar_home{
-	        width:90%;
-	       height:130px;
-	    }
-	    #contingut_home{
-	        height:auto;
-	    }
-	    #busqueda_home{
-	        width:100%;
-	        height:160px;
-	    }
-	}
+		    #ordenar_home{
+		        width:90%;
+		       height:130px;
+		    }
+		    #contingut_home{
+		        height:auto;
+		    }
+		    #busqueda_home{
+		        width:100%;
+		        height:160px;
+		    }
+		}
+		#show-ordenar_home {
+			height: 28px;
+			text-align: center;
+			background-color: lime;
+			width: 28px;
+			border-radius: 14px;
+			position: fixed;
+			z-index: 1;
+			right: 19px;
+			top: 63px;
+			
+		}
+		#show-ordenar_home:hover {
+			background-color: limegreen;
+			cursor: pointer;
+		}
 	</style>
 	<div id="contingut_home">
 	    <div class="row row-horizon">
-		    <div id='busqueda_home'>
+	    	<div id="show-ordenar_home">
+	    		*
+	    	</div>
+		    <div id='busqueda_home' style="display: none;">
 		        <div id='ordenar_home'>
 		             {{ Form::open(array('url' => '/cercarhome')) }}
 		                <fieldset>
@@ -40,7 +59,6 @@ Home
 		                    <span class='titulsbusqueda'>Tots</span> <input  name="radio1" value="tots" checked="checked" id="filtrocheckbox" type="radio">
 		                    <span class='titulsbusqueda'>Setmana</span> <input name="radio1" value="setmana" id="filtrocheckbox" type="radio">
 		                    <span class='titulsbusqueda'>Mes</span> <input name="radio1" value="mes" id="filtrocheckbox"  type="radio">
-		                    <input  style="margin-left:20px;" placeholder="tag1,tag2.." type="text" name="buscarhome" id="buscarhome">
 		                    <input type="submit" class="btn btn-default" value="Enviar">
 		                </fieldset>
 		           {{ Form::close() }}
@@ -58,6 +76,9 @@ Home
 			$('#contingut_home').height($(window).height() - $('.header').height() - $('.footer').height());
 			$(window).on('resize', function() {
 				$('#contingut_home').height($(window).height() - $('.header').height() - $('.footer').height());
+			});
+			$('#show-ordenar_home').on('click', function() {
+				$('#busqueda_home').stop().slideToggle();
 			});
 		});
 	</script>
