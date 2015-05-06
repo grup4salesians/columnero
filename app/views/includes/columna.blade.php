188a343189a0fcbcbf8608313f5955e7c1012133
sacer6 <jonatan.d.g@hotmail.com>
2015-05-06 16:30:59 +0200

    Notas

--- app/views/includes/columna.blade.php
+++ app/views/includes/columna.blade.php
@@ -3,11 +3,39 @@
         <div class="panel-heading">
             {{ $nom }}
         </div>
-
+<?php $categoria="Laravel" ?>
         <div class="panel-body">
+            <?php   //Select que coge todos las notas del usuario y el tag
+                $query = DB::table('usuaris')
+                    ->join('posts','usuaris.id','=','posts.usuari_id')
+                    ->join('postscategories','posts.id','=','postscategories.post_id')
+                    ->join('categories','postscategories.categoria_id','=','categories.id')
+                    ->where('usuaris.id','=',Auth::user()->id)
+                    ->where('categories.nom',$categoria)    
+                    ->select('postscategories.post_id','posts.titol','posts.comentari','posts.data','postscategories.categoria_id','categories.nom','usuaris.nick')
+                    ->get();
+            for($i=0;$i<count($query);$i++){
+                $titolNota = $query[$i]->titol;
+                $comentariNota = $query[$i]->comentari;
+                $nick = $query[$i]->nick;
+                
+                $queryCategories = DB::table('posts')   //Select que coge todos los tags de esa nota, porque una nota puede estar compuesta por mas de un tag
+                    ->join('postscategories','posts.id','=','postscategories.post_id')
+                    ->join('categories','postscategories.categoria_id','=','categories.id')
+                    ->where('posts.id',$query[$i]->post_id)
+                    ->select('categories.nom')
+                    ->get();
+                $categories ="";
+                for($j=0;$j<count($queryCategories);$j++){
+                    $categories= $categories .", ". $queryCategories[$j]->nom;
+                 }
+                $categories = substr($categories,2);
+            ?>   
+ 
             @include('includes/nota')
-            @include('includes/nota')
-            @include('includes/nota')
+            <?php
+               }    
+            ?>
         </div>
     </div>
 </div> 
