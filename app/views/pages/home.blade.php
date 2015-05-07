88d3a6d75cd08f88a9f9e7b51370b74e2532781a
Josep Sarabia <josep.sarabia@gmail.com>
2015-05-07 16:30:58 +0200

    no message

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -3,59 +3,70 @@
 Home
 @stop
 @section('content')
-	<div id="contingut_home">
-	    <div class="row row-horizon">
-	    	
-		    <div id='busqueda_home' style="display: none;">
-		        <div id='ordenar_home'>
-		        <i id="show-ordenar_home" class="fa fa-times"></i>
-		             {{ Form::open(array('url' => '/cercarhome')) }}
-		                <fieldset>
-		                    <legend>Ordenar per</legend>
-		                    <span class='titulsbusqueda'>Millor valorats</span> <input id="millorvalorats" name="millorvalorats" type="checkbox">
-		                    <span class='titulsbusqueda'>Tots</span> <input  name="radio1" value="tots" checked="checked" id="filtrocheckbox" type="radio">
-		                    <span class='titulsbusqueda'>Setmana</span> <input name="radio1" value="setmana" id="filtrocheckbox" type="radio">
-		                    <span class='titulsbusqueda'>Mes</span> <input name="radio1" value="mes" id="filtrocheckbox"  type="radio">
-		                    <input type="submit" class="btn btn-default" value="Enviar">
-		                </fieldset>
-		           {{ Form::close() }}
-		        </div>
-		    </div>
-	        <?php $nom = Auth::user()->getFullNameAttribute(); 
-                $queryCategories = DB::table('categories')   //Select que coge todos los tags de esa nota, porque una nota puede estar compuesta por mas de un tag
-                    ->join('categoriesusuaris','categories.id','=','categoriesusuaris.categories_id')
-                    ->join('usuaris','categoriesusuaris.usuaris_id','=','usuaris.id')
-                    ->where('usuaris.id',Auth::user()->id)
-                    ->whereNotNull('categoriesusuaris.mostrar')
-                    ->orderBy('categoriesusuaris.mostrar')
-                    ->select('categories.nom','categoriesusuaris.categories_id')
-                    ->get();
-                
-                for($j=0;$j<count($queryCategories);$j++){
-                    $categoria =$queryCategories[$j]->nom;
-                    $idCategoria = $queryCategories[$j]->categories_id;
-                ?>
-	        @include('includes/columna')
-                <?php  
-                    } 
-                    if ($j==0){
-                        echo('<input type="submit" class="btn btn-warning" value="Crear nota">');
-                    }
-                ?>
-                
-	    </div>
-	</div>
-	<script>
-		$(function() {
-			$('#contingut_home').height($(window).height() - $('.header').height() - $('.footer').height());
-			$(window).on('resize', function() {
-				$('#contingut_home').height($(window).height() - $('.header').height() - $('.footer').height());
-			});
-			$(document).on('click', '#show-ordenar_home', function() {
-				$('#busqueda_home').stop().slideToggle();
-			});
-		});
+<div id="contingut_home">
+    <div class="row row-horizon">
 
-	</script>
+        <div id='busqueda_home' style="display: none;">
+            <div id='ordenar_home'>
+                <i id="show-ordenar_home" class="fa fa-times"></i>
+                {{ Form::open(array('url' => '/cercarhome')) }}
+                <fieldset>
+                    <legend>Ordenar per</legend>
+                    <span class='titulsbusqueda'>Millor valorats</span> <input id="millorvalorats" name="millorvalorats" type="checkbox">
+                    <span class='titulsbusqueda'>Tots</span> <input  name="radio1" value="tots" checked="checked" id="filtrocheckbox" type="radio">
+                    <span class='titulsbusqueda'>Setmana</span> <input name="radio1" value="setmana" id="filtrocheckbox" type="radio">
+                    <span class='titulsbusqueda'>Mes</span> <input name="radio1" value="mes" id="filtrocheckbox"  type="radio">
+                    <input type="submit" class="btn btn-default" value="Enviar">
+                </fieldset>
+                {{ Form::close() }}
+            </div>
+        </div>
+        <?php
+        $nom = Auth::user()->getFullNameAttribute();
+        $queryCategories = DB::table('categories')   //Select que coge todos los tags de esa nota, porque una nota puede estar compuesta por mas de un tag
+                ->join('categoriesusuaris', 'categories.id', '=', 'categoriesusuaris.categories_id')
+                ->join('usuaris', 'categoriesusuaris.usuaris_id', '=', 'usuaris.id')
+                ->where('usuaris.id', Auth::user()->id)
+                ->whereNotNull('categoriesusuaris.mostrar')
+                ->orderBy('categoriesusuaris.mostrar')
+                ->select('categories.nom', 'categoriesusuaris.categories_id')
+                ->get();
+
+        for ($j = 0; $j < count($queryCategories); $j++) {
+            $categoria = $queryCategories[$j]->nom;
+            $idCategoria = $queryCategories[$j]->categories_id;
+            ?>
+            @include('includes/columna')
+            <?php
+        }
+        if ($j == 0) {
+            ?>
+
+            <div id="capnota_home">
+                <div class="row margin">
+                    <div class="col-md-12">
+                        <h4 class="page-head-line">No tens cap nota? Crea una nova!</h4>
+                    </div>
+                </div>
+                <a href="<?php Config::get('constants.BaseUrl'); ?>../public/novanota"<input type="submit" class="btn btn-warning" style="margin:auto;padding:10px;font-size:25px;margin-bottom:10px;">Crear una nova nota</a>
+            </div>
+
+        <?php } ?>
+
+
+    </div>
+</div>
+<script>
+    $(function () {
+        $('#contingut_home').height($(window).height() - $('.header').height() - $('.footer').height());
+        $(window).on('resize', function () {
+            $('#contingut_home').height($(window).height() - $('.header').height() - $('.footer').height());
+        });
+        $(document).on('click', '#show-ordenar_home', function () {
+            $('#busqueda_home').stop().slideToggle();
+        });
+    });
+
+</script>
 @stop
 
--- app/views/pages/perfilusuari.blade.php
+++ app/views/pages/perfilusuari.blade.php
@@ -13,31 +13,40 @@ Perfil Usuari
     .labelperfil{
         font-size:20px;
         text-align:left;
+        width:50px;
+    }
+    .margin{
+        margin-bottom:30px;
     }
 </style>
 <div id="contingut_home">
 
 
     <div id="Bloque_inside_perfil"><br>
-        <div class="row">
+        <div class="row margin">
             <div class="col-md-12">
                 <h4 class="page-head-line">Perfil Usuari -<?php echo $userdata[0]->nick ?>-</h4>
             </div>
         </div>
+        <div class="row margin">
+            <span class="labelperfil">Nom</span> 
+            <input type="text" class="inputperfil" value="<?php echo $userdata[0]->nom ?>" disabled="disabled">
+        </div>
+        <div class="row margin"><span class="labelperfil">Cognom</span>
+            <input type="text" class="inputperfil" value="<?php echo $userdata[0]->cognom; ?>" disabled="disabled"></div>
 
-        <span class="labelperfil">Nom</span>  <br>
-        <input type="text" class="inputperfil" value="<?php echo $userdata[0]->nom ?>" disabled="disabled"><br>
-        <span class="labelperfil">Cognom</span><br>
-        <input type="text" class="inputperfil" value="<?php echo $userdata[0]->cognom; ?>" disabled="disabled"><br>
-        <span class="labelperfil">Nick</span> <br>
-        <input type="text" class="inputperfil" value="<?php echo $userdata[0]->nick; ?>" disabled="disabled"><br>
-        <span class="labelperfil">Email</span> <br>
-        <input type="text" class="inputperfil" value="<?php echo $userdata[0]->email; ?>" disabled="disabled"><br>
-        <span class="labelperfil">Nº de posts</span> <br>
-        <input type="text" class="inputperfil" value=" <?php
-               $querypost = Post::where('usuari_id', $userdata[0]->id)->select();
-               echo count($querypost->get());
-               ?>" disabled="disabled"><br><br>          
+        <div class="row margin"><span class="labelperfil">Nick</span> 
+            <input type="text" class="inputperfil" value="<?php echo $userdata[0]->nick; ?>" disabled="disabled"></div>
+
+        <div class="row margin"> <span class="labelperfil">Email</span> 
+            <input type="text" class="inputperfil" value="<?php echo $userdata[0]->email; ?>" disabled="disabled"></div>
+
+        <div class="row margin"> <span class="labelperfil">Nº de posts</span> 
+            <input type="text" class="inputperfil" value=" <?php
+                   $querypost = Post::where('usuari_id', $userdata[0]->id)->select();
+                   echo count($querypost->get());
+                   ?>" disabled="disabled">  </div>
+
     </div>
 </div>
 
