8c6cde71abace4ea66c112653a5ca4703af1cf72
arnaupamies <apamies94@outlook.com>
2015-05-08 18:25:36 +0200

    no message

--- app/views/pages/novanota.blade.php
+++ app/views/pages/novanota.blade.php
@@ -10,12 +10,12 @@ Perfil Usuari
 <script>
     angular.module('myApp', ['ngTagsInput'])
         .controller('MyCtrl', function ($scope, $http) {
-        $scope.tags = [
-            {text: 'just'},
-            {text: 'some'},
-            {text: 'cool'},
-            {text: 'tags'}
-        ];
+//        $scope.tags = [
+//            {text: 'just'},
+//            {text: 'some'},
+//            {text: 'cool'},
+//            {text: 'tags'}
+//        ];
         $scope.loadTags = function (query) {
             return $http.get('getCategories/' + query);
         };
