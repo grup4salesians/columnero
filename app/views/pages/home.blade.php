6e9304703e8f9b270abcd57a0ddbb5708dd1d9e9
vektrom <kenzo_aku@hotmail.com>
2015-05-08 20:40:45 +0200

    PARA DE TOCAR MIS ESTILOS

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -43,7 +43,7 @@ Home
             </div>
             <div class="col-md-4 col-sm-8 col-xs-12" style="padding-top: 16%;">
                  <div class="dashboard-div-wrapper bk-clr-two">
-                    <i class="fa fa-edit dashboard-div-icon"></i>
+                    <i class="fa fa-edit dashboard-div-icon" style="font-size: 34cm;"></i>
                     <div class="progress progress-striped active">
                         <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
                         </div>
