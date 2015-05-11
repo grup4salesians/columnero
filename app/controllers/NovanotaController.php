d874103b1aaf1d79b7049ccd011fd0dc21b8113d
sacer6 <jonatan.d.g@hotmail.com>
2015-05-11 16:00:52 +0200

    INSERTAMOS NOTA, CATEGORIA, CATEGORIAUSUARIO, y demás...

--- app/controllers/NovanotaController.php
+++ app/controllers/NovanotaController.php
@@ -10,12 +10,12 @@ class NovanotaController extends BaseController {
         
          $notadata = array(
             'titol' => Input::get('Titol'),
-           // 'ListadoTags' => Input::get('ListadoTagsOculto'),
+            'ListadoTags' => Input::get('ListadoTagsOculto'),
             'TextoNota' => Input::get('TextoNota')
         );
         $rules = [
             'titol' => 'required|min:1',
-           // 'ListadoTags' => 'required|min:1',
+            'ListadoTags' => 'required|min:1',
             'TextoNota' => 'required|min:1'
         ];
         $validator = Validator::make($notadata, $rules);
@@ -33,24 +33,24 @@ class NovanotaController extends BaseController {
         $PostNuevo->comentari = $TextoNota;
         $PostNuevo->usuari_id = $ID_Usuari;
         $PostNuevo->data = date("Y-m-d H:i:s");
+        $PostNuevo->privat = 0;
         $PostNuevo->save();
         
         $idPost = $PostNuevo->id;
                 
         $ArrayTags = explode("|",$ListadoTags);
         
-        for($i=0;$i<count($ArrayTags);$i++){
+        for($i=0;$i<count($ArrayTags)-1;$i++){
             $query = DB::table('categories')
                     ->join('categoriesusuaris','categories.id','=','categoriesusuaris.categories_id')
                     ->where('categoriesusuaris.usuaris_id',$ID_Usuari)
                     ->where('categories.nom',$ArrayTags[$i])
                     ->select('categoriesusuaris.categories_id')
                     ->get();
-            //return Redirect::back()->withInput()->withErrors($query[0]->categories_id);
-            $idCategoria = $query[0]->categories_id;
+        
             if (count($query)==0){ //Si no existe la categoria, inserta en tablas; categories,CategoriesUsuaris
                 $CategoriaNueva = new Categorie();
-                $CategoriaNueva->nom = $titol;
+                $CategoriaNueva->nom = $ArrayTags[$i];
                 $CategoriaNueva->save();
                 
                 $idCategoria =  $CategoriaNueva->id; //Coge la id del que acabamos de insertar
@@ -58,13 +58,19 @@ class NovanotaController extends BaseController {
                 $CategoriaUsuarioNueva = new CategoriesUsuari();
                 $CategoriaUsuarioNueva->categories_id = $idCategoria;
                 $CategoriaUsuarioNueva->usuaris_id = $ID_Usuari;
+                $CategoriaUsuarioNueva->mostrar =0;
                 $CategoriaUsuarioNueva->save();
+            }
+            else{
+                   $idCategoria = $query[0]->categories_id;
             }
 
             $PostCategoriaNueva = new PostCategorie();
             $PostCategoriaNueva->categoria_id= $idCategoria;
             $PostCategoriaNueva->post_id = $idPost;
-            
+               
+            $PostCategoriaNueva->save();
+            //return Redirect::back()->withInput()->withErrors($ListadoTags);
         };
          return Redirect::to('/');                
        
