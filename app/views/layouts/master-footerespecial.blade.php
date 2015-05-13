f96654dde42b3f65cd77c91d6b4373f09aba5078
sacer6 <jonatan.d.g@hotmail.com>
2015-05-13 20:32:50 +0200

    footer ok en todas las webs

---
+++ app/views/layouts/master-footerespecial.blade.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>@yield('title','Index')</title>
+        @include('includes.head')
+        @yield('head')
+    </head>
+    <body>
+        <div class='header'>
+            @include('includes.header')
+        </div>
+        <div class="header-space"></div>
+        @yield('content')
+        <div class='footer FooterFixedBottom'>
+            @include('includes.footer')
+        </div>
+    </body>
+</html>
--- app/views/pages/perfil.blade.php
+++ app/views/pages/perfil.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('layouts.master-footerespecial')
 @section('title')
 Perfil
 @stop
