7e04f6ac0b8a777d41ff12c34922dd93f8e7513a
sacer6 <jonatan.d.g@hotmail.com>
2015-05-19 19:20:34 +0200

    arreglo de perfilcontroller --> contraseñas

--- app/controllers/PerfilController.php
+++ app/controllers/PerfilController.php
@@ -1,61 +1,58 @@
 <?php
 
 class PerfilController extends BaseController {
+    /*
+      |--------------------------------------------------------------------------
+      | Default Home Controller
+      |--------------------------------------------------------------------------
+      |
+      | You may wish to use controllers instead of, or in addition to, Closure
+      | based routes. That's great! Here is an example controller method to
+      | get you started. To route to this controller, just add the route:
+      |
+      |	Route::get('/', 'HomeController@showWelcome');
+      |
+     */
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+    public function CambiarPass() {
 
-        
-        public function CambiarPass(){
-          
-               $userdata = array(
+        $userdata = array(
             'passnueva' => Input::get('passnueva'),
             'passactual' => Input::get('passactual'),
             'contrasenya_confirm' => Input::get('contrasenya_confirm')
         );
-             $passactual = Auth::user()->contrasenya;
-            
-             $rules = [
-            'passnueva' => 'required|min:1',
-             'passactual' => 'required|min:1',
+
+        $rules = [
+            'passnueva' => 'required|min:5',
+            'passactual' => 'required|min:5',
             'contrasenya_confirm' => 'required|same:passnueva'
         ];
-             
+
         $validator = Validator::make($userdata, $rules);
         if ($validator->fails()) {
             return Redirect::back()->withInput()->withErrors($validator);
         }
-        
+
         $passnueva = Input::get('passnueva');
         $passnuevahash = Hash::make($passnueva);
-        
+
         $iduser = Auth::user()->id;
-        
-      Usuari::where('id','=',$iduser)->update(array('contrasenya' => $passnuevahash));
-       
-       $mensaje = array(
-           'mensaje' => 'La teva contrasenya ha sigut actualitzada.');
-       
-           return View::make('pages.perfil',$mensaje);
-        }
-        
-        public function ShowUser($nick){
-            
-          
-           
-       $userdata =   Usuari::where('nick','=',$nick)->get();
-       
+
+        Usuari::where('id', '=', $iduser)->update(array('contrasenya' => $passnuevahash));
+
+        $mensaje = array(
+            'mensaje' => 'La teva contrasenya ha sigut actualitzada.');
+
+        return View::make('pages.perfil', $mensaje);
+    }
+
+    public function ShowUser($nick) {
+
+
+
+        $userdata = Usuari::where('nick', '=', $nick)->get();
+
         return View::make('pages.perfilusuari')->with('userdata', $userdata);
-        }
+    }
 
 }
